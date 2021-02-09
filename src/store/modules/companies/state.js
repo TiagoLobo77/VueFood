6898ce9a94c49f7d7ff2fc848048d0eb3ea09d0c
TiagoLobo77 <tiago_batista77@outlook.com>
2021-02-09 01:39:43 -0300

    VueFood.modulo5.aula05

---
+++ src/store/modules/companies/state.js
@@ -0,0 +1,17 @@
+const state = {
+    items: {
+        data: []
+    },
+    companySelected: {
+        name: '',
+        products: {
+            data: []
+        }
+    },
+
+    categoriesCompanySelected: {
+        data: []
+    }
+}
+
+export default state
